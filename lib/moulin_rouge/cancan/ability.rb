5d511a71b4d7bf5988ae7e168a71ee3cc1d5eb54
Edson Hilios <ehilios@corp.hotwords.com>
2012-03-15 15:21:35 -0300

    Protecting the namespaces inside Ability

--- lib/moulin_rouge/cancan/ability.rb
+++ lib/moulin_rouge/cancan/ability.rb
@@ -6,16 +6,16 @@ module MoulinRouge
       include ::CanCan::Ability
       # Define all permissions collect by MoulinRouge
       def initialize(model)
-        MoulinRouge.reload! unless MoulinRouge.configuration.cache
+        ::MoulinRouge.reload! unless MoulinRouge.configuration.cache
         # Set all permissions in main
-        MoulinRouge::Permission.main.abilities.each do |ability|
+        ::MoulinRouge::Permission.main.abilities.each do |ability|
           ability.send_to(self)
         end
         # Set all permissions by roles
-        MoulinRouge::Permission.all.each do |role, permission|
+        ::MoulinRouge::Permission.all.each do |role, permission|
           permission.abilities.each do |ability|
             ability.send_to(self)
-          end if model.send(MoulinRouge.configuration.test_method, role)
+          end if model.send(::MoulinRouge.configuration.test_method, role)
         end
       end
     end
