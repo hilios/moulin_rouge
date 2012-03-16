0d420a489789c338c18ac054a88396ac1b39dfd3
Edson Hilios <edson.hilios@gmail.com>
2012-03-15 23:55:39 -0300

    Implementting the tests for the model double, and making more broader

--- lib/moulin_rouge/model_double.rb
+++ lib/moulin_rouge/model_double.rb
@@ -3,7 +3,7 @@ module MoulinRouge
     # Return an block for later evaluation
     def method_missing(name, *args, &block)
       lambda do |scope|
-        scope.send(name.to_sym)
+        scope.send(name.to_sym, *args, &block)
       end
     end
   end
---
+++ spec/moulin_rouge/model_double_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe MoulinRouge::ModelDouble do
+  let(:model) { ModelDouble.new }
+  
+  it "returns an proc for every method called in this class" do
+    model.id.should be_a(Proc)
+    model.abc.should be_a(Proc)
+  end
+  
+  it "the proc sends the methods to the given scope with all arguments" do
+    object = double(:id => true)
+    object.should_receive(:id).with(:this).once
+    model.id(:this).call(object)
+  end
+end
